0508937b99af0e17b4e42faaa10abe429e27994d
maxi <by.mikky1206@gmail.com>
2025-08-26 19:58:19 +0300

    обновление страницы настроек, добавление настройки пути к бд

--- src/pages/settings_page.py
+++ src/pages/settings_page.py
@@ -11,6 +11,49 @@ class SettingsPage(BasePage):
     Страница настроек приложения.
     Позволяет управлять автосохранением, темой, экспортом/импортом данных и сбросом настроек.
     """
+    def __init__(self, app):
+        super().__init__(app)
+        # Настройка интерфейса
+        self.dark_mode_switch = ft.Switch(label="Темная тема", value=self.app.settings.interface.dark_mode,
+                                          on_change=self._dark_mode_change)
+        # Выбор файлового сервера
+        self.path_file_server_text_field = ft.TextField(label="Файловый сервер",
+                                                        value=self.app.settings.paths.file_server,
+                                                        on_change=lambda e: self._reset_text_error(
+                                                            self.path_file_server_text_field),
+                                                        expand=True)
+        self.path_file_server_button = ft.IconButton(icon=ft.Icons.FOLDER_OPEN, icon_size=24, tooltip="Выбрать путь",
+                                                     on_click=lambda e: self._select_dir_action(
+                                                         self.path_file_server_text_field, 'file_server'))
+        # Выбор папки проектов
+        self.path_projects_folder_text_field = ft.TextField(label="Папка объектов",
+                                                            value=self.app.settings.paths.projects_folder,
+                                                            on_change=lambda e: self._reset_text_error(
+                                                                self.path_file_server_text_field),
+                                                            expand=True)
+        self.path_projects_folder_button = ft.IconButton(icon=ft.Icons.FOLDER_OPEN, icon_size=24,
+                                                         tooltip="Выбрать путь",
+                                                         on_click=lambda e: self._select_dir_action(
+                                                             self.path_projects_folder_text_field, 'projects_folder'))
+        # Подключение базы данных
+        self.path_database_text_field = ft.TextField(label="Путь к базе данных",
+                                                     value=self.app.settings.paths.database_path,
+                                                     on_change=lambda e: self._reset_text_error(
+                                                         self.path_file_server_text_field),
+                                                     expand=True)
+        self.path_database_file_button = ft.IconButton(icon=ft.Icons.FOLDER_OPEN, icon_size=24, tooltip="Выбрать путь",
+                                                       on_click=lambda e: self._select_file_action(
+                                                           self.path_database_text_field, 'database_path'))
+
+    def _dark_mode_change(self, e):
+        if self.dark_mode_switch.value:
+            self.app.settings.interface.dark_mode = True
+            self.app.page.theme_mode = "dark"
+        else:
+            self.app.settings.interface.dark_mode = False
+            self.app.page.theme_mode = "light"
+        self.app.save_settings()
+        self.app.page.update()
 
     def _select_dir_action(self, obj: Any, parameter: str) -> None:
         """Добавляет новую директорию в закрепленные"""
@@ -18,20 +61,24 @@ class SettingsPage(BasePage):
         def on_result(e: ft.FilePickerResultEvent):
             if e.path:
                 obj.value = e.path
-                self._save_parameter(parameter, e.path)
 
         pick_files_dialog = ft.FilePicker(on_result=on_result)
         self.page.overlay.append(pick_files_dialog)
         self.page.update()
         pick_files_dialog.get_directory_path()
 
-    def _save_parameter(self, parameter: str, value: str | int | float) -> None:
-        match parameter:
-            case 'file_server':
-                self.app.settings.paths.file_server = value
-            case 'projects_folder':
-                self.app.settings.paths.projects_folder = value
-        self.app.save_settings()
+    def _select_file_action(self, obj: Any, parameter: str) -> None:
+        def on_result(e: ft.FilePickerResultEvent):
+            if e.path:
+                obj.value = e.path
+
+        pick_files_dialog = ft.FilePicker(on_result=on_result)
+        self.page.overlay.append(pick_files_dialog)
+        self.page.update()
+        pick_files_dialog.pick_files()
+
+    def _reset_text_error(self, obj: Any) -> None:
+        obj.error_text = None
         self.page.update()
 
     def get_content(self):
@@ -39,34 +86,6 @@ class SettingsPage(BasePage):
         Возвращает содержимое страницы настроек (UI).
         :return: Flet Column с элементами интерфейса
         """
-        # Настройка интерфейса
-        def dark_mode_change(e):
-            if dark_mode_switch.value:
-                self.app.settings.interface.dark_mode = True
-                self.app.page.theme_mode = "dark"
-            else:
-                self.app.settings.interface.dark_mode = False
-                self.app.page.theme_mode = "light"
-            self.app.save_settings()
-            self.app.page.update()
-        dark_mode_switch = ft.Switch(label="Темная тема", value=self.app.settings.interface.dark_mode,
-                                     on_change=dark_mode_change)
-
-        # Выбор файлового сервера
-        paths_file_server_text_field = ft.TextField(label="Файловый сервер", value=self.app.settings.paths.file_server,
-                                                    expand=True)
-        paths_file_server_text_field.on_submit = lambda _: self._save_parameter('file_server', paths_file_server_text_field.value)
-        paths_file_server_button = ft.IconButton(icon=ft.Icons.FOLDER_OPEN, icon_size=24,
-                                                 tooltip="Выбрать путь",
-                                                 on_click=lambda e: self._select_dir_action(paths_file_server_text_field, 'file_server'))
-        # Выбор папки проектов
-        paths_projects_folder_text_field = ft.TextField(label="Папка объектов",
-                                                        value=self.app.settings.paths.projects_folder,
-                                                        expand=True)
-        paths_projects_folder_text_field.on_submit = lambda _: self._save_parameter('projects_folder', paths_file_server_text_field.value)
-        paths_projects_folder_button = ft.IconButton(icon=ft.Icons.FOLDER_OPEN, icon_size=24,
-                                                     tooltip="Выбрать путь",
-                                                     on_click=lambda e: self._select_dir_action(paths_projects_folder_text_field, 'projects_folder'))
         return ft.Column([
             ft.Text("Настройки", size=24, weight=ft.FontWeight.BOLD),
             ft.Divider(height=20),
@@ -78,19 +97,25 @@ class SettingsPage(BasePage):
             ft.Divider(height=20),
             
             ft.Column([
-                ft.Text("Настройки интерфейса", size=18, weight=ft.FontWeight.BOLD),
-                dark_mode_switch
+                ft.Text("Интерфейс", size=18, weight=ft.FontWeight.BOLD),
+                self.dark_mode_switch
             ]),
 
             ft.Column([
-                ft.Text("Настройки путей", size=18, weight=ft.FontWeight.BOLD),
-                ft.Row([paths_file_server_text_field, paths_file_server_button]),
-                ft.Row([paths_projects_folder_text_field, paths_projects_folder_button]),
+                ft.Text("Файловый сервер", size=18, weight=ft.FontWeight.BOLD),
+                ft.Row([self.path_file_server_text_field, self.path_file_server_button]),
+                ft.Row([self.path_projects_folder_text_field, self.path_projects_folder_button]),
             ]),
 
-            # ...
+            ft.Column([
+                ft.Text("База данных", size=18, weight=ft.FontWeight.BOLD),
+                ft.Row([self.path_database_text_field, self.path_database_file_button]),
+            ]),
 
-            ft.ElevatedButton("Сброс настроек", icon=ft.Icons.RESTORE, on_click=self.reset_settings),
+            ft.Row([
+                ft.ElevatedButton("Сброс настроек", icon=ft.Icons.RESTORE, on_click=self.reset_settings),
+                ft.ElevatedButton("Применить", icon=ft.Icons.CHECK, on_click=self.apply)
+            ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
         ])
     
     def reset_settings(self, e=None):
@@ -101,3 +126,34 @@ class SettingsPage(BasePage):
         self.app.settings.init_default_settings()
         self.app.save_settings()
         self.app.show_info("Выполнен сброс настроек приложения к значениям по умолчанию")
+
+    def apply(self, e=None):
+        """
+        Применение настроек приложения
+        :param e: Событие нажатия кнопки
+        """
+        def check_dir(dirpath: str | Path) -> bool:
+            return Path(dirpath).is_dir()
+
+        def check_file(filepath: str | Path) -> bool:
+            return Path(filepath).exists()
+
+        if check_dir(self.path_file_server_text_field.value):
+            self.app.settings.paths.file_server = self.path_file_server_text_field.value
+            self.app.save_settings()
+        else:
+            self.path_file_server_text_field.error_text = "Неверный путь"
+
+        if check_dir(self.path_projects_folder_text_field.value):
+            self.app.settings.paths.projects_folder = self.path_projects_folder_text_field.value
+            self.app.save_settings()
+        else:
+            self.path_projects_folder_text_field.error_text = "Неверный путь"
+
+        if check_file(self.path_database_text_field.value):
+            self.app.settings.paths.database_path = self.path_database_text_field.value
+            self.app.save_settings()
+        else:
+            self.path_database_text_field.error_text = "Неверный путь"
+
+        self.app.page.update()
